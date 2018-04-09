9c4dd4ac44c19c3e24681c94a01c3e24b85e8376
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-09 18:05:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,27 +13,20 @@ def vgg16():
 
 	vgg16_model = keras.applications.vgg16.VGG16()
 
-	#vgg16_model.summary()
-
-
 	model = Sequential()
 	for layer in vgg16_model.layers:
 		model.add(layer)
 	
 	model.layers.pop()
-	model.layers.pop()
-	model.layers.pop()
-
+	
+	model.add(Dense(2, activation=None))
+	
 	for layer in model.layers:
-		layer.trainable = False
-	
-	model.add(Dense(6, activation=None))
-	
+		layer.trainable = True
+		
 	model.summary()
 	return model
 
-
-	
 
 
 
@@ -58,17 +51,13 @@ if __name__ == "__main__":
 
 	np.random.seed(7) # for reproducibility
 
-
-
 	batch_size = 14
 
 	#model = load_model('vgg16_edit.h5')
 	model = vgg16()
-	model.load_weights('trained_model_weights.h5')
-
 
 	y_filename ='./data/data.txt'
-	y_data = np.loadtxt(y_filename, delimiter='  ')
+	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1])
 
 	y_data_train = y_data[:]
 
@@ -85,15 +74,14 @@ if __name__ == "__main__":
 	#adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 	#adam = Adam(lr=0.0001)
 	adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-	#sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 	model.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
 
-
+	epochs=1
 	# Train:
 	print('Start training ...')
 	start = time.time()
 	hist = model.fit(x = x_data_train, y = y_data_train,
-		  epochs=5,
+		  epochs=1,
 		  batch_size=batch_size, validation_split = 0.20, shuffle = True, verbose = 1)  
 		  #By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
 	end = time.time()
@@ -103,12 +91,33 @@ if __name__ == "__main__":
 	model.save_weights('trained_model_weights.h5')
 	model.save('trained_model.h5')
 
-	#utils.eml_save_history(hist, metrics)
+	train_loss = hist.history['loss']
+	val_loss = hist.history['val_loss']
+	train_acc = hist.history['acc']
+	val_acc = hist.history['val_acc']
+	
+	xc = range(epochs)
+	
+	plt.figure(1,figsize=(7,5))
+	plt.plot(xc,train_loss)
+	plt.plot(xc,val_loss)
+	plt.xlabel('num of Epochs')
+	plt.ylabel('loss')
+	plt.title('train_loss vs val_loss')
+	plt.grid(True)
+	plt.legend(['train','val'])
+	print plt.style.available # use bmh, classic,ggplot for big pictures
+	plt.style.use(['classic'])
+	plt.savefig('train_loss vs val_loss.png')
 
-
-
-
-
-
-
-
+	plt.figure(2,figsize=(7,5))
+	plt.plot(xc,train_acc)
+	plt.plot(xc,val_acc)
+	plt.xlabel('num of Epochs')
+	plt.ylabel('accuracy')
+	plt.title('train_acc vs val_acc')
+	plt.grid(True)
+	plt.legend(['train','val'],loc=4)
+	#print plt.style.available # use bmh, classic,ggplot for big pictures
+	plt.style.use(['classic'])
+	plt.savefig('train_acc vs val_acc')
