5c5c2e0bc41ff8d695beafd373aca073db32c722
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-11 13:29:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,79 @@ if __name__ == "__main__":
 
 	#model = load_model('vgg16_edit.h5')
 	model = vgg16()
-	model.summary()
 	#model.load_weights('trained_model_weights.h5')
+
+	y_filename ='./data/data.txt'
+	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1])
+
+	y_data_train = y_data[:]
+
+	#########################################
+
+	h5f = h5py.File('images_in_h5_format.h5','r')
+	x_data_train = h5f['dataset_1'][:]
+
+
+
+	# ======================================================================                     
+	# Configure the training process:
+	print('Preparing training ...')
+	#adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+	#adam = Adam(lr=0.0001)
+	adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0, amsgrad=False)
+	model.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
+	
+	#update
+	'''
+	filepath="best_model.hdf5"	
+	checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='max')
+	callbacks_list = [checkpoint] 
+	'''
+
+	iter=50
+	# Train:
+	print('Start training ...')
+	start = time.time()
+	history = model.fit(x = x_data_train, y = y_data_train,
+		  epochs=iter,
+		  batch_size=batch_size, validation_split = 0.20, shuffle = True, verbose = 1)  
+		  #By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
+	end = time.time()
+	print ("Model took %0.2f seconds to train"%(end - start))
+
+
+	model.save_weights('trained_model_weights.h5')
+	#model.save('trained_model.h5')
+	
+	print(history.history.keys()) 
+
+	plt.figure(1)  
+
+	# summarize history for accuracy  
+
+	plt.subplot(211)  
+	plt.plot(history.history['acc'])  
+	plt.plot(history.history['val_acc'])  
+	plt.title('model accuracy')  
+	plt.ylabel('accuracy')  
+	plt.xlabel('epoch')  
+	plt.legend(['train', 'validation'], loc='upper left')  
+
+	# summarize history for loss  
+
+	plt.subplot(212)  
+	plt.plot(history.history['loss'])  
+	plt.plot(history.history['val_loss'])  
+	plt.title('model loss')  
+	plt.ylabel('loss')  
+	plt.xlabel('epoch')  
+	plt.legend(['train', 'validation'], loc='upper left')  
+	#plt.show()
+	plt.savefig('visualization1.png')
+
+	#update
+	'''
+	loss_history = history
+	numpy_loss_history = np.array(loss_history)
+	np.savetxt("history.txt", numpy_loss_history, delimiter=",")
+	'''
