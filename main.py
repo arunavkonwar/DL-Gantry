69cfc35d7f0448828c9cc9cae6e33bba9d2393ae
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-17 10:47:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 
 	#model = load_model('vgg16_edit.h5')
 	model = vgg16()
-	#model.load_weights('/local/akonwar/trained_weights/full_train_1-20_after_sgd_valid_freeze1-20_8k_1-10.h5')
+	model.load_weights('/local/akonwar/trained_weights/noweights_8k_1-108k_1-10.h5')
 	
 	y_filename ='./data/data_8k.txt'
 	
@@ -113,7 +113,7 @@ if __name__ == "__main__":
 	callbacks_list = [checkpoint] 
 	'''
 
-	iter=10
+	iter=5
 	# Train:
 	print('Start training ...')
 	start = time.time()
@@ -148,10 +148,10 @@ if __name__ == "__main__":
 	plt.xlabel('epoch')  
 	plt.legend(['train', 'validation'], loc='upper left')  
 	#plt.show()
-	plt.savefig('visualization_noweights_8k_1-10.png')
+	plt.savefig('visualization_noweights_8k_10-15.png')
 
 
-	model.save_weights('/local/akonwar/trained_weights/noweights_8k_1-108k_1-10.h5')
+	model.save_weights('/local/akonwar/trained_weights/noweights_8k_1-108k_10-15.h5')
 	#model.save('trained_model.h5')
 	
 	
