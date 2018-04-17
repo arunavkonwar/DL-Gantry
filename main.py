d1ef0a0579d15551b928e5b6c6a12a78c799ace7
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-17 18:35:59 +0200

    test mode

--- main.py
+++ main.py
@@ -117,10 +117,14 @@ if __name__ == "__main__":
 	# Train:
 	print('Start training ...')
 	start = time.time()
+	'''
 	history = model.fit(x = x_data_train, y = y_data_train,
 		  epochs=iter,
 		  batch_size=batch_size, validation_data = ( x_data_valid, y_data_valid ), shuffle = True, verbose = 1)  
 		  #By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
+	'''
+	history = evaluate(self, x=x_data_train, y=y_data_train, batch_size=50, verbose=1, sample_weight=None, steps=5)
+	
 	end = time.time()
 	print ("Model took %0.2f seconds to train"%(end - start))
 	
