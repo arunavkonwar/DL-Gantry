29164642a3432079311dcc260687b83399a364a4
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-19 19:38:03 +0200

    Update other0.py

--- other_architectures/other/other0.py
+++ other_architectures/other/other0.py
@@ -15,33 +15,24 @@ def vgg16():
 
 	weights_path=None
 	
-	model = models.Sequential()
-	model.add(ZeroPadding2D((1,1),input_shape=(224,224,3)))
-	model.add(Conv2D(64, 3, 3, activation='relu'))
-	model.add(Conv2D((1,1)))
-	model.add(Conv2D(64, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+	input_shape = (224, 224, 3)
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Conv2D(128, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Conv2D(128, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+	model = Sequential([
+	    Conv2D(64, (3, 3), input_shape=input_shape, padding='same', activation='relu'),
+	    Conv2D(64, (3, 3), activation='relu', padding='same'),
+	    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+	    Conv2D(128, (3, 3), activation='relu', padding='same'),
+	    Conv2D(128, (3, 3), activation='relu', padding='same',),
+	    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+	    Conv2D(256, (3, 3), activation='relu', padding='same',),
+	    Conv2D(256, (3, 3), activation='relu', padding='same',),
+	    Conv2D(256, (3, 3), activation='relu', padding='same',),
+	    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+	    Flatten(),
+	    Dense(2, activation='linear')
+	])
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Conv2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Conv2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Conv2D(256, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(Flatten())
-	#model.add(Dense(4096, activation='relu'))
-	#model.add(Dropout(0.5))
-	#model.add(Dense(4096, activation='relu'))
-	#model.add(Dropout(0.5))
-	model.add(Dense(2, activation='linear'))
+	model.summary()
 
 	if weights_path:
 		model.load_weights(weights_path)
