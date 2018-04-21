d92eccf1b137a21907a69d78a65dd4c8c8acd797
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-21 22:04:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -99,11 +99,10 @@ if __name__ == "__main__":
 	batch_size = 14
 
 	model = vgg16()
-	model.load_weights('/local/akonwar/trained_weights/trained_model_works_full_vgg_april21.h5')
+	#model.load_weights('/local/akonwar/trained_weights/trained_model_works_full_vgg_april21.h5')
 	
 	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_8k.txt'
 	
-	#y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_40k.txt'
 	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1])
 	y_data_train = y_data[:]
 	#########################################
@@ -111,8 +110,6 @@ if __name__ == "__main__":
 	#for 8k images dataset
 	h5f = h5py.File('/local/akonwar/image_data/images_in_h5_format_8k.h5','r')
 	
-	#for 40k images dataset
-	#h5f = h5py.File('/local/akonwar/image_data/images_in_h5_format_40k.h5','r')
 	x_data_train = h5f['dataset_1'][:]
 	
 	h5f = h5py.File('/local/akonwar/image_data/validation_images_in_h5_format_8k.h5','r')
@@ -127,7 +124,6 @@ if __name__ == "__main__":
 	# ======================================================================                     
 	# Configure the training process:
 	print('Preparing training ...')
-	#adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 	sgd = SGD(lr=1e-6, momentum=0.9, decay=0.001, nesterov=False)	
 	#adam = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
@@ -150,18 +146,19 @@ if __name__ == "__main__":
 		  epochs=iter,
 		  batch_size=batch_size, validation_data = ( x_data_valid, y_data_valid ), shuffle = True, verbose = 1)  
 		  #By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
-	
+	#test mode
 	#score = model.evaluate(x=x_data_train, y=y_data_train, batch_size=50, verbose=1, sample_weight=None, steps=None)
 	
-	end = time.time()
-	print ("Model took %0.2f seconds to train"%(end - start))
-	
-	print(history.history.keys()) 
 	#for test mode
 	'''
 	print('Test loss:', score[0])
 	print('Test accuracy:', score[1])
 	'''
+	
+	end = time.time()
+	print ("Model took %0.2f seconds to train"%(end - start))
+	
+	print(history.history.keys()) 
 	
 	# summarize history for accuracy 
 	plt.figure(1)  
