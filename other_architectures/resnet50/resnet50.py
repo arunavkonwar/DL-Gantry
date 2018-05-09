510a00dd6b03ce3d5126791ef15c7c014bcc7750
Arunav Konwar <arunavkonwar@gmail.com>
2018-05-09 21:34:39 +0200

    resnet50_90percent_1-150_adam_0001_3axis_high_res

--- other_architectures/resnet50/resnet50.py
+++ other_architectures/resnet50/resnet50.py
@@ -70,25 +70,25 @@ if __name__ == "__main__":
 	model = vgg16()
 	#model.load_weights('/local/akonwar/trained_weights/trained_model_resnet50_90percent_1-50_adam_0001_high_res.h5')
 	
-	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_3axis.txt'
+	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_3axis_high_res.txt'
 	
-	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,2])
+	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1,2])
 	y_data_train = y_data[:]
 	#########################################
 	
 	#for 8k images dataset
 	#h5f = h5py.File('/local/akonwar/image_data/images_in_h5_format_8k.h5','r')
 	#h5f = h5py.File('/local/akonwar/image_data/images_in_h5_format_8k_by255.h5','r')
-	h5f = h5py.File('/local/akonwar/image_data/3axis.h5','r')
+	h5f = h5py.File('/local/akonwar/image_data/3axis_high_res.h5','r')
 	
 	x_data_train = h5f['dataset_1'][:]
 	
 	#h5f = h5py.File('/local/akonwar/image_data/validation_images_in_h5_format_8k.h5','r')
-	h5f = h5py.File('/local/akonwar/image_data/3axis.h5','r')
+	h5f = h5py.File('/local/akonwar/image_data/3axis_high_res.h5','r')
 	x_data_valid = h5f['dataset_1'][:]
 	
-	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_3axis.txt'
-	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,2])
+	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data_3axis_high_res.txt'
+	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1,2])
 	y_data_valid = y_data[:]
 
 
@@ -153,7 +153,7 @@ if __name__ == "__main__":
 	plt.xlabel('epoch')  
 	plt.legend(['train', 'validation'], loc='upper left')  
 	#plt.show()
-	plt.savefig('visualization_resnet50_90percent_1-100_adam_0001_3axis.png')
+	plt.savefig('visualization_resnet50_90percent_1-150_adam_0001_3axis_high_res.png')
 
 
-	model.save_weights('/local/akonwar/trained_weights/trained_model_resnet50_90percent_1-100_adam_0001_3axis.h5')
+	model.save_weights('/local/akonwar/trained_weights/trained_model_resnet50_90percent_1-150_adam_0001_3axis_high_res.h5')
